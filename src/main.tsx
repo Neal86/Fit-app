2885bad1b3ec269983032d599cfa0325f29b20b6
Neal86 <52608308+Neal86@users.noreply.github.com>
2026-07-31 23:39:45 -0700

    style: load runtime recovery styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import './runtime.css'
 
 class ErrorBoundary extends React.Component<React.PropsWithChildren, { hasError: boolean }> {
   state = { hasError: false }
